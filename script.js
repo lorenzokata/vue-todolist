bcc31db35397e4a82b9dbcada938878a62a84f08
lorenzokata <catalanolorenzo95@gmail.com>
2021-06-24 02:03:51 +0200

    end

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const app = new Vue(
       
       data:{
         todoList:['FARE LA SPESA', 'ANDARE IN PALESTRA', 'ORGANIZZARE LE VACANZE', 'FARE APERITIVO', 'MEDITARE', 'LEGGERE', 'STUDIARE'],
-        counter: 0,
         task: ""
       },
   
       methods:{
-        addTodo(){
+        addTask(){
             (this.task) ? this.todoList.push(this.task.toUpperCase()) : "";
             this.task = ""
+        },
+
+        removeTask(index){
+            this.todoList.splice(index,1)
         }
       }
     }
